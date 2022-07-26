16a210ba1a983914379b5fd8cab27dc30c5dfef2
Muhammed Wael <muhammed2110669@miuegypt.edu.eg>
2022-07-26 21:42:25 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "**********************************\n";
+    std::cout << "*     programming Exercise       *\n";
+    std::cout << "*     computer Programming       *\n";
+    std::cout << "*          Author:???            *\n";
+    std::cout << "*     Date: Saturday, Oct.24     *\n";
+    std::cout << "**********************************\n";
+
+    return 0;
+}
