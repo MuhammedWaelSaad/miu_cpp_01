531f6af70412dd7bf49cd0d766016785969afe31
Muhammed Wael <muhammed2110669@miuegypt.edu.eg>
2022-07-30 17:26:15 +0200

    program6

---
+++ main 4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main()
+{
+char x;
+std::cout << "Enter: " << std::endl;
+std::cin >> x;
+
+if (x == 'b' )
+    {
+        std::cout << " b " << std::endl;
+    }
+else
+{
+    std::cout << "This character is not b" << std:: endl;
+}
+    return 0;
+}
